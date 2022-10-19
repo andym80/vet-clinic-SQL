320494aedf72fffd504d3f1538f3302747df793d
Andy Menutti <andymenutti@gmail.com>
2022-10-19 15:37:17 -0300

    insert new values

--- data.sql
+++ data.sql
@@ -1,11 +1,22 @@
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (1,'Agumon', '2020/02/03', 0, false, 10.23);
 
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) 
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (2,'Gabumon', '2018/11/18', 2, true, 8);
 
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) 
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (3,'Pikachu', '2021/01/07', 1, false, 15.04);
 
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) 
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (4,'Devimon', '2017/05/12', 5, true, 11);
+
+--insert multiple values
+
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES (5,'Charmander','2020/2/8',0,false,11),
+(6,'Plantmon','2021/11/15',2,true,5.7),
+(7,'Squirtle','1993/4/2',3,false,12.13),
+(8,'Angemon','2005/6/12',1,false,45),
+(9,'Boarmon','2005/6/7',7,true,20.4),
+(10,'Blossom','1998/10/13',3,true,17),
+(11,'Ditto','2022/5/14',4,true,22);
